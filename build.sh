cc0cbf84241bcaa271548f44c1325b3ce9f97311
Daniel Flynn <daniel.flynn@stileeducation.com>
2022-11-27 16:14:13 +1100

    Build script.

---
+++ build.sh
@@ -0,0 +1,8 @@
+# Unit tests to check functionality
+# python3 -m unittest -v src/sandbox_test
+
+# Run the main program
+# python3 scrmabled-strings.py
+python3 src/sandbox.py
+# ./scrmabled-strings --dictionary [PATH TO DICTIONARY FILE] --input [PATH TO INPUT
+# FILE]
