79cd0efef8dc4eecef652865ef3db46a6197c189
Daniel Flynn <daniel.flynn@stileeducation.com>
2022-11-27 22:11:36 +1100

    Program description and basic main area.

---
+++ src/scrmabled_strings.py
@@ -0,0 +1,16 @@
+"""
+-- SCRMABLED-STRINGS --
+This program accepts a user-specified dictionary file and a list of
+strings, then identifies all of the occurences of dictionary words 
+(as well as their scrambled permutations) in each string.
+Results are printed to an output file.
+"""
+
+
+def main():  # Main program
+    pass
+
+
+# When to actually run the main program
+if __name__ == "__main__":
+    main()
